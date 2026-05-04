d9ca8f67a2ec8661bc9a67d34cf3ad69d25e807d
Andrew <andrewthcook@gmail.com>
2026-05-04 13:55:04 -0400

    hello

--- hello.py
+++ hello.py
@@ -1,4 +1,1 @@
-print("Hello from both GitHub and local computer!")
-print("This version combines both edits")
-print("Local version: edited locally")
-print("GitHub version: edited on GitHub")
+# comment
